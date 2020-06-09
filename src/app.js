95af575fd03305faff5a36ebf45a333934f035c9
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-06-09 19:04:20 +0100

    split app into their own component files

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
-// // import './utils.js';
-// import subtract, { square, add } from './utils'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import IndecisionApp from './components/IndecisionApp'
 
-// console.log('app.js is running')
-
-// console.log(square(2))
-// console.log(add(2,4))
-// console.log(subtract(100, 10))
-
-import isSenior, {isAdult, canDrink} from './person'
-
-console.log(isAdult(21))
-console.log(canDrink(12))
-console.log(isSenior(65))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
--- src/person.js
+++
@@ -1,7 +0,0 @@
-const isAdult = (age) => age >= 18
-
-const canDrink = (age) => age >= 21
-
-export default (age) => age >= 65
-
-export { isAdult, canDrink }
