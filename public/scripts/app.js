4c0d97fc989aa73befd913ce9c52fac161b577c9
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-06-09 16:06:26 +0100

    persist data with local storage and clear input field on submission

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -32,12 +32,26 @@ var IndecisionApp = function (_React$Component) {
     _createClass(IndecisionApp, [{
         key: 'componentDidMount',
         value: function componentDidMount() {
-            console.log('fetching data');
+            try {
+                var json = localStorage.getItem('options');
+                var options = JSON.parse(json);
+
+                if (options) {
+                    this.setState(function () {
+                        return { options: options };
+                    });
+                }
+            } catch (e) {
+                // Do nothing
+            }
         }
     }, {
         key: 'componentDidUpdate',
         value: function componentDidUpdate(prevProps, prevState) {
-            console.log('saving data');
+            if (prevState.options.length !== this.state.options.length) {
+                var json = JSON.stringify(this.state.options);
+                localStorage.setItem('options', json);
+            }
         }
     }, {
         key: 'componentWillUnmount',
@@ -159,17 +173,18 @@ var Options = function Options(props) {
             { onClick: props.handleDeleteOptions },
             'Remove All'
         ),
-        React.createElement(
-            'h3',
+        props.options.length === 0 && React.createElement(
+            'p',
             null,
-            props.options.map(function (option) {
-                return React.createElement(Option, {
-                    key: option,
-                    optionText: option,
-                    handleDeleteOption: props.handleDeleteOption
-                });
-            })
-        )
+            'Please add an option to get started: '
+        ),
+        props.options.map(function (option) {
+            return React.createElement(Option, {
+                key: option,
+                optionText: option,
+                handleDeleteOption: props.handleDeleteOption
+            });
+        })
     );
 };
 
@@ -215,6 +230,10 @@ var AddOption = function (_React$Component2) {
             this.setState(function () {
                 return { error: error };
             });
+
+            if (!error) {
+                e.target.elements.option.value = '';
+            }
         }
     }, {
         key: 'render',
--- src/app.js
+++ src/app.js
@@ -14,11 +14,25 @@ class IndecisionApp extends React.Component {
     }
 
     componentDidMount() {
-        console.log('fetching data')
+        try {
+            const json = localStorage.getItem('options')
+            const options = JSON.parse(json)
+            
+            if (options) {
+                this.setState(() => ({ options }))
+            }
+        } catch (e) {
+            // Do nothing
+        }
+
+
     }
 
     componentDidUpdate(prevProps, prevState) {
-        console.log('saving data')
+        if (prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options)
+            localStorage.setItem('options', json)
+        }
     }
 
     componentWillUnmount() {
@@ -109,7 +123,8 @@ const Options = (props) => {
     return(
         <div>
         <button onClick={props.handleDeleteOptions}>Remove All</button>
-            <h3>{
+        {props.options.length === 0 && <p>Please add an option to get started: </p>}
+            {
                 props.options.map((option) => (
                     <Option 
                         key={option} 
@@ -117,7 +132,7 @@ const Options = (props) => {
                         handleDeleteOption={props.handleDeleteOption}
                     />
                 ))
-            }</h3>
+            }
         </div> 
     )
 }
@@ -152,6 +167,10 @@ class AddOption extends React.Component {
         const error = this.props.handleAddOption(option)
 
         this.setState(() => ({ error }))
+
+        if (!error) {
+            e.target.elements.option.value = ''
+        }
     }
     render() {
         return(
