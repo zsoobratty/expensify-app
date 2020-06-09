5633940888e2c3a53a33c7d412bcdab7e16e4888
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-06-09 19:20:03 +0100

    implement development server

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -11,6 +11,7 @@ export default class AddOption extends React.Component {
     
     handleAddOption(e) {
         e.preventDefault();
+        console.log('testing')
         const option = e.target.elements.option.value.trim()
         const error = this.props.handleAddOption(option)
 
