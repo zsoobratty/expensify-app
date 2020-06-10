f29e186cc2bc0780f5ed05d67a532bf300ecf780
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-06-10 17:37:00 +0100

    add modal for decision button

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,10 +3,16 @@ import AddOption from './AddOption'
 import Action from './Action'
 import Header from './Header'
 import Options from './Options'
+import OptionModal from'./OptionModal'
 
 export default class IndecisionApp extends React.Component {
     state = {
-        options: []
+        options: [],
+        selectedOption: undefined
+    }
+
+    handleClearSelectedOption = () => {
+        this.setState(() => ({ selectedOption: undefined }))
     }
 
     handleDeleteOptions = () => {
@@ -22,7 +28,9 @@ export default class IndecisionApp extends React.Component {
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length)
         const option = this.state.options[randomNum]
-        alert(option)
+        this.setState(() => ({
+            selectedOption: option
+        }))
     }
 
     handleAddOption = (option) => {
@@ -60,7 +68,7 @@ export default class IndecisionApp extends React.Component {
     }
 
     render() {
-        const subtitle = "We'll decide for you!"
+        const subtitle = "We'll take it from here!"
 
         return(
             <div>
@@ -76,6 +84,10 @@ export default class IndecisionApp extends React.Component {
                 />
                 <AddOption 
                     handleAddOption={this.handleAddOption}
+                />
+                <OptionModal 
+                    selectedOption={this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
                 />
             </div>
         )
