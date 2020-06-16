46b57226d17f474dc3ee50f8bb4ecac489213bbd
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-06-16 13:40:29 +0100

    array destructuring in playground

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object Destructuring:
+//
+
 // const person = {
 //     name: 'Ziad',
 //     age: 90,
@@ -17,15 +21,28 @@
 //     console.log(`It's ${temperature} degrees celsius in ${city}`)
 // }
 
-const book = {
-    title: 'Harry Potter',
-    author: 'J K Rowling',
-    publisher: {
-        name: 'Penguin'
-    }
-}
+// const book = {
+//     title: 'Harry Potter',
+//     author: 'J K Rowling',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// }
 
-const {name: publisherName = 'Self-published'} = book.publisher
+// const {name: publisherName = 'Self-published'} = book.publisher
 
 
-console.log(publisherName)
+// console.log(publisherName)
+
+//
+// Array Destructuring:
+//
+
+const address = ['10 Downing Street', 'London', 'United Kingdom', 'SW1A 2AA']
+const [, city, country = 'UK'] = address
+console.log(`You are in ${city} ${country}`)
+
+const item = ['Coffee (iced)', '£3.00', '£3.50', '£3.75']
+const [itemName, , medium] = item
+
+console.log(`A medium ${itemName} costs ${medium}`)
