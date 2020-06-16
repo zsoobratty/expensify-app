8c03d9965e62639d0bad87da4b1b03ace31a3590
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-06-16 14:08:46 +0100

    destructuring and refactoring counter playground

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,16 +1,37 @@
 import { createStore } from 'redux'
 
+// Action generators - functions that return action objects
+
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({ 
+    type: 'INCREMENT',
+    incrementBy
+})
+
+const decrementCount = ( { decrementBy = 1 } = {} ) => ({
+    type: 'DECREMENT',
+    decrementBy
+})
+
+const resetCount = () => ({
+    type: 'RESET'
+})
+
+const setCount = ( { count } ) => ({
+    type: 'SET',
+    count
+})
+
+
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             };
         case 'SET':
             return {
@@ -35,26 +56,23 @@ const unsubscribe = store.subscribe(() => {
 
 // I'd like to increment the count
 
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-})
+// store.dispatch({
+//     type: 'INCREMENT',
+//     incrementBy: 5
+// })
+
+store.dispatch(incrementCount({ incrementBy: 5 }))
+
+store.dispatch(incrementCount())
 
 // I'd like to reset the count
 
-store.dispatch({
-    type: 'RESET'
-})
+store.dispatch(resetCount())
 
 // I'd like to decrement the count
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 10
-})
+store.dispatch(decrementCount())
+store.dispatch(decrementCount({ decrementBy: 10 }))
 
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-})
+store.dispatch(setCount( { count: 101 } ))
