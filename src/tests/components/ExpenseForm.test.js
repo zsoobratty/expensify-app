b6de293b7ea3bfd7bc867855d44ca14d2cfed4fe
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-06-25 17:03:59 +0100

    add test for onSubmit with props form submission

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -58,3 +58,18 @@ test('should not set amount if invalid input', () => {
     })
     expect(wrapper.state('amount')).toBe("")
 })
+
+test('should call onSubmit prop for valid form submission', () => {
+    const onSubmitSpy = jest.fn()
+    const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy}/>)
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => { }
+    })
+    expect(wrapper.state('error')).toBe('')
+    expect(onSubmitSpy).toHaveBeenLastCalledWith({
+        description: expenses[0].description,
+        amount: expenses[0].amount,
+        note: expenses[0].note,
+        createdAt: expenses[0].createdAt
+    })
+})
