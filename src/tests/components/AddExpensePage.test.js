7954ededf1cbaec8cf57201a6d9bf63314f3e79f
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-06-25 17:34:43 +0100

    refactor from onSubmit to addExpense

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -3,11 +3,11 @@ import { shallow } from 'enzyme'
 import { AddExpensePage } from '../../components/AddExpensePage'
 import expenses from '../fixtures/expenses'
 
-let onSubmit, history, wrapper;
+let addExpense, history, wrapper;
 beforeEach(() => {
-    onSubmit = jest.fn();
+    addExpense = jest.fn();
     history = { push: jest.fn() }
-    wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+    wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
 })
 
 test('should render AddExpensePage correctly', () => {
@@ -17,5 +17,5 @@ test('should render AddExpensePage correctly', () => {
 test('should handle onSubmit', () => {
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[0])
     expect(history.push).toHaveBeenLastCalledWith('/')
-    expect(onSubmit).toHaveBeenLastCalledWith(expenses[0])
+    expect(addExpense).toHaveBeenLastCalledWith(expenses[0])
 })
