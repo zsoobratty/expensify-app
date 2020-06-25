105af395cae5f69c1b945d7a6f0d5d04117c3aba
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-06-25 17:32:46 +0100

    refactor tests and handle onSubmit test

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { AddExpensePage } from '../../components/AddExpensePage'
+import expenses from '../fixtures/expenses'
+
+let onSubmit, history, wrapper;
+beforeEach(() => {
+    onSubmit = jest.fn();
+    history = { push: jest.fn() }
+    wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+})
 
 test('should render AddExpensePage correctly', () => {
-    const onSubmit = jest.fn();
-    const history = { push: jest.fn() }
-    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
     expect(wrapper).toMatchSnapshot()
+})
+
+test('should handle onSubmit', () => {
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[0])
+    expect(history.push).toHaveBeenLastCalledWith('/')
+    expect(onSubmit).toHaveBeenLastCalledWith(expenses[0])
 })
