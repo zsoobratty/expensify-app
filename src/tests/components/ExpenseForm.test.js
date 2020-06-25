fc0279273adf935ceb289581a042d26a5ea92547
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-06-25 17:15:27 +0100

    add test for onFocusChange

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -81,3 +81,10 @@ test('should set new date on date change', () => {
     wrapper.find('SingleDatePicker').prop('onDateChange')(now)
     expect(wrapper.state('createdAt')).toEqual(now)
 })
+
+test('should set calendar focus on change', () => {
+    const focused = true
+    const wrapper = shallow(<ExpenseForm />)
+    wrapper.find('SingleDatePicker').prop('onFocusChange')({ focused })
+    expect(wrapper.state('calendarFocused')).toBe(focused)
+})
