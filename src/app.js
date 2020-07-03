1d13a4f2330aa35da21865945b8d80e73583bc94
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-07-03 15:05:24 +0100

    tweak webpack config for build

--- src/app.js
+++ src/app.js
@@ -3,24 +3,12 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense, removeExpense, editExpense} from './actions/expenses'
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters'
-import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css' 
 
 
 const store = configureStore();
-
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 500, createdAt: 500 } ))
-store.dispatch(addExpense({ description: 'Gas bill', amount: 2500, createdAt: 9000 } ))
-store.dispatch(addExpense({ description: 'Electricity bill', amount: 9000, createdAt: 1500 } ))
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
 
 const jsx = (
     <Provider store={store}>
